434ed84f7cca5206a997e47ee7a4af97a50b8cbe
unknown <robert.kami88@gmail.com>
2020-10-26 19:02:28 +0100

    Rpn release

--- src/lib/Plugins/Language/NumeralsToDigit/Tokenizer/tokens/Idle.ts
+++
@@ -1,6 +0,0 @@
-import {IPosition, IToken} from "../Token";
-
-export default class Idle implements IToken {
-    constructor(public position: IPosition, public value: string) {
-    }
-}
--- src/lib/Plugins/Language/NumeralsToDigit/Tokenizer/tokens/numerals/Numeral.ts
+++
@@ -1,3 +0,0 @@
-import {Token} from "../../Token";
-
-export default class Numeral extends Token{}
