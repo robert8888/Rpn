e52331340d8042e0156169276c6a4e7c012e98df
unknown <robert.kami88@gmail.com>
2020-11-16 11:17:27 +0100

    Comments for rpn index file

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -9,30 +9,34 @@ import FunctionCalculator, {ArgsInput} from "./Processes/FunctionCalculator/Func
 export {default as Processor} from "./Processor/Processor";
 export {IProcess} from "./Process/Process";
 
-export default class Rpn extends Processor{
+export default class Rpn{
     private computes = new Map<string, IExpression>();
     private lastInput: string | undefined;
     private functionCalculator : FunctionCalculator | undefined;
 
-    constructor() {
-        super();
-        this.use(new Parser());
-        this.use(new Converter());
-        this.use(new Calculator());
+    private processor: IProcessor;
+    //if process are not give then it use default configuration
+    constructor(processes: IProcess[] = [new Parser(), new Converter(), new Calculator()]) {
+        this.processor = new Processor();
+        processes.forEach((process: IProces) => {
+            this.processor.use(process)
+        })
     }
-
+    //transform input expression string to expression object
     private expression(input: string): IExpression{
         this.lastInput = input;
         if(!this.computes.has(input)){
-            this.computes.set(input, <IExpression>(this.compute(input)))
+            this.computes.set(input, <IExpression>(this.processor.compute(input)))
         }
         return <IExpression>(this.computes.get(input));
     }
 
+    //return value of expression
     public valueOf(input: string): number | boolean | null {
         return (<IExpression>this.expression(input)).value;
     }
 
+    //return values for args array (mathematical function arguments) for computed expression or string expression
     public valuesOf(args: ArgsInput, input?:string | IExpression){
         if(typeof input === "string"){
             input = <IExpression>this.expresion(input);
@@ -50,13 +54,16 @@ export default class Rpn extends Processor{
         return this.functionCalculator.valuesOf(input, args)
     }
 
+    //return built expression from string mathematical expression to computed
     public expresionFrom(input: string){
         return this.expression(input);
     }
 
+    //return last computed expression
     public get last(): IExpression{
         return <IExpression>this.computes.get(<string>this.lastInput);
     }
+    //return value of last computed expression
     public get lastValue(): number | boolean | null {
         return this.last.value;
     }
