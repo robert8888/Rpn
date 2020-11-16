1b79779240ef93850b749bfda1ef1c31ccb5a853
unknown <robert.kami88@gmail.com>
2020-11-16 08:40:39 +0100

    remove build folder

--- build/Processes/Process.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Traceable = void 0;
-class Traceable {
-    constructor() {
-        this.id = this.constructor.name;
-    }
-}
-exports.Traceable = Traceable;
